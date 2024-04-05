5591201752d868c4ba65f36ca20ad0453d7531cf
Catalina Londoño <nilocuru@hotmail.com>
2024-04-05 18:26:15 -0500

    Finished A4

--- A4/chatrooma4/lib/chat.ex
+++ A4/chatrooma4/lib/chat.ex
@@ -1,9 +1,79 @@
 use CSP
 
+defmodule Chat do
 
-defmodule chat do
+  def handle(channel, state) do
+    message = Channel.get(channel)
+    state = case message do
+      {:join, user} -> add_user(user, state)
+      {:exit, user} -> user_delete(user, state)
+      {:write, user, message} -> write_message(message, user, state)
+      {:read, user} -> read_messages(user, state)
+      _ -> state
+    end
+    handle(channel, state)
+  end
 
 
-  channel = Channel.new
-  def add_u
+
+
+  def add_user(state, user) do
+
+    user_list = state |> Map.get(:users)
+
+    if user_list |> Enum.member?(user) do
+      IO.puts("SERVER: User #{user} already exists in the chatroom")
+      state
+    else
+      state = state |> Map.update!(:users, &[user | &1])
+      IO.puts("SERVER: User #{user} has joined the chatroom")
+      state
+    end
+  end
+
+  def user_delete(state, user) do
+
+    user_list = state |> Map.get(:users)
+
+    if user_list |> Enum.member?(user) do
+      state = state |> Map.update!(:users, &List.delete(&1, user))
+      IO.puts("SERVER: User #{user} has left the chatroom")
+      state
+    else
+      IO.puts("SERVER: User #{user} does not exist in the chatroom")
+      state
+    end
+  end
+
+  def write_message(state, user, message) do
+
+    user_list = state |> Map.get(:users)
+
+    if user_list |> Enum.member?(user) do
+      state = state |> Map.update!(:messages, &["#{user}: #{message}" | &1])
+      IO.puts("SERVER: Message from #{user} has been added to the chatroom")
+      state
+    else
+      IO.puts("SERVER: User #{user} does not exist in the chatroom")
+      state
+    end
+  end
+
+  def read_messages(state, user) do
+
+    user_list = state |> Map.get(:users)
+
+    if user_list |> Enum.member?(user) do
+      messages = state |> Map.get(:messages)
+      IO.puts("*******************************")
+      IO.puts("SERVER: Messages in chatroom:")
+      Enum.each(messages, &IO.puts(&1))
+      IO.puts("*******************************")
+      state
+    else
+      IO.puts("SERVER: User #{user} does not exist in the chatroom")
+      state
+    end
+  end
+
 end
--- A4/chatrooma4/lib/chatrooma4.ex
+++ A4/chatrooma4/lib/chatrooma4.ex
@@ -1,18 +1,57 @@
+use CSP
+
 defmodule Chatrooma4 do
-  @moduledoc """
-  Documentation for `Chatrooma4`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
+  def main do
+    channel = Channel.new
+    {:ok, pid} = Task.start_link(fn -> Chat.handle(channel, %{users: [], messages: []}) end)
+    run(channel)
+  end
 
-      iex> Chatrooma4.hello()
-      :world
+  def run(channel) do
+    IO.puts("--------------------------------------------------")
+    IO.puts("Welcome to the chat room manager with CSP!")
+    IO.puts("Please enter your command: ")
+    IO.puts("1. Join chatroom")
+    IO.puts("2. Delete user from chatroom")
+    IO.puts("3. Write message")
+    IO.puts("4. Read messages")
+    IO.puts("--------------------------------------------------")
+    command = IO.gets("") |> String.trim()
 
-  """
-  def hello do
-    :world
+    case command do
+      "1" -> add_user(channel)
+      "2" -> user_delete(channel)
+      "3" -> write_message(channel)
+      "4" -> read_messages(channel)
+      _ -> IO.puts("Invalid command")
+    end
+  end
+
+  def add_user (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:join, user})
+  end
+
+  def user_delete (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:exit, user})
+  end
+
+  def write_message (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    IO.puts("Enter your message: ")
+    message = IO.gets("") |> String.trim()
+    Channel.put(channel, {:write, user, message})
+  end
+
+  def read_messages (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:read, user})
   end
 end
