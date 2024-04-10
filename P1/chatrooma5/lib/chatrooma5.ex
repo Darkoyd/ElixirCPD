c208ec500cf773b69130dd5da615010decb12c2c
Catalina Londoño <nilocuru@hotmail.com>
2024-04-10 02:25:46 -0500

    Started P1

---
+++ P1/chatrooma5/lib/chatrooma5.ex
@@ -0,0 +1,53 @@
+defmodule Chatrooma5 do
+  def main(chat) do
+    spawn(__MODULE__, :chat, chat)
+  end
+
+
+  def run(channel) do
+    IO.puts("--------------------------------------------------")
+    IO.puts("Welcome to the chat room manager with CSP!")
+    IO.puts("Please enter your command: ")
+    IO.puts("1. Join chatroom")
+    IO.puts("2. Delete user from chatroom")
+    IO.puts("3. Write message")
+    IO.puts("4. Read messages")
+    IO.puts("--------------------------------------------------")
+    command = IO.gets("") |> String.trim()
+
+    case command do
+      "1" -> add_user(channel)
+      "2" -> user_delete(channel)
+      "3" -> write_message(channel)
+      "4" -> read_messages(channel)
+      _ -> IO.puts("Invalid command")
+    end
+    run(channel)
+  end
+
+  def add_user (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:join, user})
+  end
+
+  def user_delete (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:exit, user})
+  end
+
+  def write_message (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    IO.puts("Enter your message: ")
+    message = IO.gets("") |> String.trim()
+    Channel.put(channel, {:write, user, message})
+  end
+
+  def read_messages (channel) do
+    IO.puts("Enter your username: ")
+    user = IO.gets("") |> String.trim()
+    Channel.put(channel, {:read, user})
+  end
+end
---
+++ P1/chatrooma5/test/chatrooma5_test.exs
@@ -0,0 +1,8 @@
+defmodule Chatrooma5Test do
+  use ExUnit.Case
+  doctest Chatrooma5
+
+  test "greets the world" do
+    assert Chatrooma5.hello() == :world
+  end
+end
